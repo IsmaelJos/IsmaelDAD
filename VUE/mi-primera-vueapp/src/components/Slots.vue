fb987ad9f97408ce16ff977365a16cc72251ca0b
IsmaelJos <ismaelejano@gmail.com>
2024-12-02 14:35:10 +0000

    feat:

---
+++ VUE/mi-primera-vueapp/src/components/Slots.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Titulo</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque assumenda porro officia incidunt commodi eveniet, natus quaerat numquam velit, ex debitis suscipit molestias distinctio beatae hic adipisci at, repellat odit.</p>
+  <template>
+    
+  </template>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
