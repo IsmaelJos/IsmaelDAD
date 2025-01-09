416a5898112a4da70240a7a648d1171d5dd0d560
IsmaelJos <ismaelejano@gmail.com>
2025-01-09 17:59:15 +0000

    feat:

---
+++ VUE/actividad2/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <ArcaneComponent/>
+</template>
+
+<script setup>
+import ArcaneComponent from './components/ArcaneComponent.vue'
+</script>
+
+<style>
+</style>
---
+++ VUE/actividad2/src/components/ArcaneComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cuerpo">
+    <form>
+      <input type="text" name="Nombre" id="NombreID">
+      <input type="text" name="Origen" id="OrigenID">
+      <input type="text" name="HabilidadEspecial" id="HabilidadEspecialID">
+      <input type="text" name="Aliados" id="AliadosID">
+      <input type="checkbox" name="Mision" id="MisionID">
+    </form>
+      
+</div>
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+    export default defineComponent({
+        name: 'ArcaneComponent',
+
+        data(){
+          let listaPersonajes = [];
+
+        },
+        methods:{
+          pillarInfo(){
+
+          }
+        }
+    })
+
+</script>
+
+<style scoped>
+
+</style>
